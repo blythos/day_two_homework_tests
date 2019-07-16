2bbb76ee63a6a6ad11c7db1a0363119c08142339
Stephen Blythe <stephenblythe@gmail.com>
2019-07-16 15:31:30 +0100

    First 14 tests complete.

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -61,3 +61,34 @@ def number_to_full_month_name(month)
     return "Incorrect input"
   end
 end
+
+def number_to_short_month_name(month)
+  case month
+  when 1
+    return "Jan"
+  when 2
+    return "Feb"
+  when 3
+    return "Mar"
+  when 4
+    return "Apr"
+  when 5
+    return "May"
+  when 6
+    return "Jun"
+  when 7
+    return "Jul"
+  when 8
+    return "Aug"
+  when 9
+    return "Sept"
+  when 10
+    return "Oct"
+  when 11
+    return "Nov"
+  when 12
+    return "Dec"
+  else
+    return "Incorrect input"
+  end
+end
